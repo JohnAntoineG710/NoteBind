e6d718fba96dced630f8533d9df6e07140041482
John Antoine G <john.antoine.g@gmail.com>
2021-04-27 23:25:10 +0200

    Edit Default Notes

--- src/notes.js
+++ src/notes.js
@@ -6,6 +6,20 @@ const notes = [
       "This is a sample note. You can perform CRUD operations on notes and folders, and everything is saved to local storage. Please note that this application is intended as a showcase, and should not be used in production.",
     folderID: 1,
   },
+  {
+    key: 2,
+    title: "Application Theme",
+    content:
+      "Switch between light and dark themes using the theme button in the upper right corner",
+    folderID: 1,
+  },
+  {
+    key: 3,
+    title: "GitHub",
+    content:
+      "Check out the project on GitHub, click the GitHub Icon in the upper right corner.",
+    folderID: 1,
+  },
 ];
 
 export default notes;
